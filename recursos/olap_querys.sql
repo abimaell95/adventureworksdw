54bbc00f9d1f18a33a2def2e850b4ae578a2eb5a
Camilo Gutiérrez <37206064+abimaell95@users.noreply.github.com>
2022-02-02 11:26:56 -0500

    Update olap_querys.sql

--- recursos/olap_querys.sql
+++ recursos/olap_querys.sql
@@ -1,3 +1,15 @@
 select englishmonthname, count(englishmonthname) total  from DIMDATE group by englishmonthname
+
 select englishdaynameofweek, count(englishdaynameofweek) as total from  dimdate d group by englishdaynameofweek
+
 select englishdaynameofweek, count(englishdaynameofweek) as total  from  dimdate where monthnumberofyear=12 group by englishdaynameofweek
+
+
+select CALENDARQUARTER, count(CALENDARQUARTER) QUARTER_TOTAL,
+CASE
+    WHEN CALENDARQUARTER = 1 THEN 'Q1'
+    WHEN CALENDARQUARTER = 2 THEN 'Q2'
+    WHEN CALENDARQUARTER = 3 THEN 'Q3'
+    WHEN CALENDARQUARTER = 4 THEN 'Q4'
+END AS CALENDARQUARTER_NAME
+from DIMDATE group by CALENDARQUARTER 
